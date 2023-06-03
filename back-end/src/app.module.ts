125a13e9683b470d32c2f51ded66fa82c2fbf010
yousefwahba <yousefwahba.official@gmail.com>
2023-06-04 00:59:37 +0300

    connect to database

--- back-end/src/app.module.ts
+++ back-end/src/app.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+      isGlobal: true,
+    }),
+    MongooseModule.forRoot(process.env.DB_URI),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
