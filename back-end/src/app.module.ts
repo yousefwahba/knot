5758d3e52144680013db898e53aedbc2c6191ce2
yousefwahba <yousefwahba.official@gmail.com>
2023-06-04 19:06:03 +0300

    setup user and auth module

--- back-end/src/app.module.ts
+++ back-end/src/app.module.ts
@@ -3,6 +3,8 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule } from '@nestjs/config';
+import { UserModule } from './user/user.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
@@ -11,6 +13,8 @@ import { ConfigModule } from '@nestjs/config';
       isGlobal: true,
     }),
     MongooseModule.forRoot(process.env.DB_URI),
+    UserModule,
+    AuthModule,
   ],
   controllers: [AppController],
   providers: [AppService],
