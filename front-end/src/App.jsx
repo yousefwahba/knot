19773079868a379876e9c871889fcefe82c51a75
yousefwahba <yousefwahba.official@gmail.com>
2023-06-07 20:30:02 +0300

    add Flowbite-tailwind css components framework

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,6 +1,15 @@
+import { Button } from 'flowbite-react';
+
 function App() {
   return (
-    <h1 className="text-3xl font-bold underline text-red-400">Hello world!</h1>
+    <div className="h-[100vh] w-full bg-gray-600 flex flex-col justify-center items-center text-center">
+      <h1 className="text-3xl font-bold  text-red-400 mb-2">
+        Hello knot ! <br />
+      </h1>
+      <div>
+        <Button className="text-sm p-0">Click me</Button>
+      </div>
+    </div>
   );
 }
 
